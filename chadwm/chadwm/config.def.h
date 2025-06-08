46219b54c953f19b8e8ef6aa841737ee8bde6c16
Nicol van der Merwe <nicol@evsolutions.biz>
2025-06-08 13:21:19 +0200

    Fixed bar icon size

--- chadwm/chadwm/config.def.h
+++ chadwm/chadwm/config.def.h
@@ -37,7 +37,7 @@ static const int new_window_attach_on_end = 0; /*  1 means the new window will a
 #define ICONSIZE 19   /* icon size */
 #define ICONSPACING 8 /* space between icon and title */
 
-static const char *fonts[]          = {"UbuntoMono Nerd Font:style:medium:size=12" ,"JetBrainsMono Nerd Font Mono:style:medium:size=19" };
+static const char *fonts[]          = { "Ubuntu Nerd Font:style:medium:size=12:antialias=true:autohint=true" };
 
 // theme
 #include "themes/tokyonight.h"
