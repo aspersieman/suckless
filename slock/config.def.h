7b604b3bcc4eb21e3f18c1abe05cb3fac3f5c41d
Nicol van der Merwe <nicol@evsolutions.biz>
2025-06-17 15:34:47 +0200

    slock patch: colormessage

--- slock/config.def.h
+++ slock/config.def.h
@@ -10,3 +10,13 @@ static const char *colorname[NUMCOLS] = {
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
+
+/* default message */
+static const char * message = "Suckless: Software that sucks less.";
+
+/* text color */
+static const char * text_color = "#ffffff";
+
+/* text size (must be a valid size) */
+// Use /usr/bin/xlsfonts to get a list of available fonts
+static const char * font_name = "lucidasanstypewriter-bold-24";
