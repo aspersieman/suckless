a1d059c966d5f95f528f8e1ebdc70d617dd0484e
Nicol van der Merwe <nicol@evsolutions.biz>
2025-07-15 08:37:41 +0200

    New patch
     - New calendar bar button
     - st: patch: boxdraw
     - New calcure dependancy added to install

--- chadwm/install.sh
+++ chadwm/install.sh
@@ -1,5 +1,6 @@
 echo "Install dependencies..."
-sudo apt install feh libimlib2-dev picom feh acpi rofi xclip maim dash x11-xserver-utils light xbacklight blueman yad
+sudo apt install feh libimlib2-dev picom feh acpi rofi xclip maim dash x11-xserver-utils light xbacklight blueman yad pipx
+pipx install calcure
 
 echo " TODO: Set up Rofi from here https://github.com/adi1090x/rofi ..."
 
